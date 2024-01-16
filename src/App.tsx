1a21bb479a34630e19b98f650b39e2bde8acfa4f
Vipin kumar <vipin.kumar@mavenwave.com>
2024-01-16 13:11:08 +0530

    contact app created

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import AddEdit from "./components/AddEdit/AddEdit";
+import Home from "./components/Home/Home";
+import Info from "./components/Info/info";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <ToastContainer />
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/addContact" element={<AddEdit />} />
+          <Route path="/editContact/:id" element={<AddEdit />} />
+          <Route path="/info/:id" element={<Info />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
